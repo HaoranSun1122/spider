d1672abfddbac81ece47f46ba10b79a792f5d447
HaoranSun1122 <65561262+HaoranSun1122@users.noreply.github.com>
2021-05-31 17:12:37 +0800

    finish

--- vehicle2.py
+++ vehicle2.py
@@ -1,17 +1,11 @@
-import math
 from scipy import integrate
 from sympy import *
-import matplotlib.pyplot as pl
-import numpy as np
-from pynverse import inversefunc
-import scipy.stats as st
-from scipy.optimize import root
+
 
 class Vehicle(object):
-    # 增加一些属性，比如t0，tr，执行时间，
-    # 添加一个方法，在tr时刻后速度保持不变，加速度为0
     def __init__(self, name: str, inital_speed: int, T_initial: int):
         self.name = name
+        # Speed limit
         if inital_speed > 30:
             print("The speed is too high")
             raise ValueError
@@ -20,7 +14,7 @@ class Vehicle(object):
             raise ValueError
         else:
             self.inital_speed = inital_speed
-
+        # Entry time must not be less than 0
         if T_initial < 0:
             print("The initial is error")
             raise ValueError
@@ -30,53 +24,32 @@ class Vehicle(object):
     def get_VehicleName(self):
         return self.name
 
+    # The moment when the reordering starts
     def get_T_resequencing(self):
         T_resequencing = 200 / self.inital_speed + self.T_initial
         T_resequencing = round(T_resequencing, 2)
         print('T_resequencing is ', T_resequencing)
         return T_resequencing
 
+    # The moment of reaching the meeting point at a constant speed
     def get_Tm_Uniform_speed(self):
         Tm_Uniform_speed = 500 / self.inital_speed + self.T_initial
         float(Tm_Uniform_speed)
         Tm_Uniform_speed = round(Tm_Uniform_speed, 2)
-        print('Tm_Uniform_speed is', Tm_Uniform_speed)
         return Tm_Uniform_speed
 
-    # def get_T_during_resequencing(self):
-    #     T_during_resequencing = 0
-    #     # 变速所需时间
-    #     return T_during_resequencing
-    #
-    # def get_acceleration(self):
-    #     # 加速度
-    #     acceleration = 0
-    #     return acceleration
-
+    # Optimal speed after sorting
     def get_final_speed(self):
         final_speed = self.inital_speed + self.get_acceleration() * self.get_T_during_resequencing()
         return final_speed
 
+    # Time to reach the meeting point after sorting
     def get_Tm(self):
         Tm = self.T_initial + self.get_T_during_resequencing() + 200 / self.get_final_speed()
         return Tm
 
-    # def function_of_energy_consumption_uniform_speed(self):
-    #     return self.inital_speed * self.inital_speed * (self.get_Tm_Uniform_speed() - self.get_T_resequencing())
-    #
-    # def get_Energy_Consumption_Uniform_speed(self):
-    #     x = symbols('x')
-    #     Energy_Consumption_Uniform_speed = integrate(self.function_of_energy_consumption_uniform_speed(), (x, self.get_T_resequencing(), self.get_Tm_Uniform_speed())).evalf()
-    #     Energy_Consumption_Uniform_speed = round(Energy_Consumption_Uniform_speed, 2)
-    #     print("Energy_Consumption_Uniform_speed is", Energy_Consumption_Uniform_speed)
-    #     return Energy_Consumption_Uniform_speed
 
-# def energy_consumption(vehicle, List_Main, List_Ramp, List_All):
-#     if List_Main[1].
-#     return
-
-
-
+# Instantiate the class, assuming 5 cars
 Vehicle_Main_1 = Vehicle('Vehicle_Main_1', 20, 0)
 Vehicle_Main_2 = Vehicle('Vehicle_Main_2', 25, 7)
 Vehicle_Ramp_1 = Vehicle('Vehicle_Ramp_1', 23, 3)
@@ -84,26 +57,7 @@ Vehicle_Ramp_2 = Vehicle('Vehicle_Ramp_2', 22, 5)
 Vehicle_Ramp_3 = Vehicle('Vehicle_Ramp_3', 22, 7)
 
 
-
-# List_Main = []
-# # 主干道车辆
-# List_Ramp = []
-# # 匝道车辆
-List_All = []
-# List_Main.append(Vehicle_Main_1)
-# List_Main.append(Vehicle_Main_2)
-# List_Ramp.append(Vehicle_Ramp_1)
-# List_Ramp.append(Vehicle_Ramp_2)
-# List_Ramp.append(Vehicle_Ramp_3)
-#
-#
-# if List_Main[0].get_T_resequencing() < List_Ramp[0].get_T_resequencing():
-#     List_All.append(List_Main[0])
-#     List_Main.remove(List_Main[0])
-# else:
-#     List_All.append(List_Ramp[0])
-#     List_Ramp.remove(List_Ramp[0])
-
+# List of all vehicles in
 List_All = []
 List_All.append(Vehicle_Main_1)
 List_All.append(Vehicle_Main_2)
@@ -111,9 +65,7 @@ List_All.append(Vehicle_Ramp_1)
 List_All.append(Vehicle_Ramp_2)
 List_All.append(Vehicle_Ramp_3)
 
-for i in range(len(List_All)):
-    print(List_All[i].get_Tm_Uniform_speed())
-
+# Quick sort, according to the time of arrival at the meeting point
 def partition(arr, low, high):
     i = (low - 1)  # Minimum element index
     pivot = arr[high].get_Tm_Uniform_speed()
@@ -143,16 +95,7 @@ def quickSort(arr, low, high):
 n = len(List_All)
 quickSort(List_All, 0, n - 1)
 
-
-# result of the sorting
-print("After sorting:")
-for i in range(n):
-    print(i, ' Vehicle\'s name:', List_All[i].name,'      Time of arrive merge point:', List_All[i].get_Tm_Uniform_speed())
-
-for i in range(len(List_All)):
-    print(List_All[i].get_Tm_Uniform_speed())
-
-# 匀速情况下能量消耗
+# Energy consumption at constant speed
 def function_of_energy_consumption_uniform_speed(inital_speed, Tm_Uniform_speed, T_resequencing):
     return inital_speed * inital_speed * (Tm_Uniform_speed - T_resequencing)
 
@@ -163,7 +106,7 @@ def get_Energy_Consumption_Uniform_speed(inital_speed, Tm_Uniform_speed, T_reseq
     print("Energy_Consumption_Uniform_speed is", Energy_Consumption_Uniform_speed)
     return Energy_Consumption_Uniform_speed
 
-# 计算加速度方法
+# Calculation method of acceleration
 def Calculate_acceleration(initial_speed, Target_time, Resequence_time):
     during_resequencing = 3
     distance = 300
@@ -171,7 +114,7 @@ def Calculate_acceleration(initial_speed, Target_time, Resequence_time):
     acceleration = round(acceleration, 2)
     return acceleration
 
-
+# Energy consumption when acceleration changes
 def function_of_energy_consumption(initial_speed, Resequence_time, acceleration):
     during_resequencing = 3
     Control_Zone_Distance = 300 - (initial_speed * during_resequencing + 0.5 * acceleration * during_resequencing * during_resequencing)
@@ -189,16 +132,16 @@ def get_Energy_Consumption(initial_speed, Resequence_time, acceleration, Target_
 
 print('--------------------------------------')
 
-# List_All内的第二辆车能量消耗
+# Energy consumption of the second car in List_All，Include overtaking and non-overtaking
 Energy_Consumption_1 = []
 
-# 不变的情况下能量消耗
+# Energy consumption without overtaking
 Energy_Consumption_1.append(get_Energy_Consumption_Uniform_speed(List_All[1].inital_speed, List_All[1].get_Tm_Uniform_speed(), List_All[1].get_T_resequencing()))
 print('Energy_Consumption_1', Energy_Consumption_1)
 
-# 超越前车情况下能量消耗
-# 对第2辆车进行判断能量消耗
-# 加速度
+# Energy consumption in the case of overtaking the preceding vehicle
+# Judge the energy consumption of the second car
+# Calculate acceleration
 acceleration_1 = []
 acceleration = Calculate_acceleration(List_All[1].inital_speed, List_All[0].get_Tm_Uniform_speed(), List_All[1].get_T_resequencing())
 acceleration_1.append(acceleration)
@@ -207,15 +150,15 @@ Energy_Consumption_1.append(get_Energy_Consumption(List_All[2].inital_speed, Lis
 print('Energy_Consumption_1', Energy_Consumption_1)
 
 print('--------------------------------------')
-# List_All内的第三辆车能量消耗
+# Energy consumption of the third car in List_All
 Energy_Consumption_2 = []
-# 不变的情况下能量消耗
+# Energy consumption without change
 
 Energy_Consumption_2.append(get_Energy_Consumption_Uniform_speed(List_All[1].inital_speed, List_All[1].get_Tm_Uniform_speed(), List_All[1].get_T_resequencing()))
 print('Energy_Consumption_2', Energy_Consumption_2)
-# 超越前车情况下能量消耗
-# 对第2辆车进行判断能量消耗
-# 加速度
+# Energy consumption in the case of overtaking the preceding vehicle
+# # Judging the energy consumption of the second car
+# # Acceleration
 acceleration_2 = []
 acceleration = Calculate_acceleration(List_All[2].inital_speed, List_All[0].get_Tm_Uniform_speed(), List_All[2].get_T_resequencing())
 acceleration_2.append(acceleration)
@@ -230,14 +173,14 @@ Energy_Consumption_2.append(get_Energy_Consumption(List_All[2].inital_speed, Lis
 print(Energy_Consumption_2)
 
 print('--------------------------------------')
-# List_All内的第四辆车能量消耗
+# List_All in the fourth car energy consumption
 Energy_Consumption_3 = []
-# 不变的情况下能量消耗
+# Energy consumption without change
 Energy_Consumption_3.append(get_Energy_Consumption_Uniform_speed(List_All[3].inital_speed, List_All[3].get_Tm_Uniform_speed(), List_All[3].get_T_resequencing()))
 print('Energy_Consumption_3', Energy_Consumption_3)
-# 超越前车情况下能量消耗
-# 对第2辆车进行判断能量消耗
-# 加速度
+# Energy consumption in the case of overtaking the preceding vehicle
+# Judging the energy consumption of the second car
+# Acceleration
 acceleration_3 = []
 acceleration = Calculate_acceleration(List_All[3].inital_speed, List_All[0].get_Tm_Uniform_speed(), List_All[3].get_T_resequencing())
 acceleration_3.append(acceleration)
@@ -258,14 +201,14 @@ Energy_Consumption_3.append(get_Energy_Consumption(List_All[3].inital_speed, Lis
 print(Energy_Consumption_3)
 
 print('--------------------------------------')
-# List_All内的第五辆车能量消耗
+# List_All the energy consumption of the fifth car in
 Energy_Consumption_4 = []
-# 不变的情况下能量消耗
+# Energy consumption without change
 Energy_Consumption_4.append(get_Energy_Consumption_Uniform_speed(List_All[4].inital_speed, List_All[4].get_Tm_Uniform_speed(), List_All[4].get_T_resequencing()))
 print('Energy_Consumption_4', Energy_Consumption_4)
-# 超越前车情况下能量消耗
-# 对第2辆车进行判断能量消耗
-# 加速度
+# Energy consumption in the case of overtaking the preceding vehicle
+# Judging the energy consumption of the second car
+# Acceleration
 acceleration_4 = []
 acceleration = Calculate_acceleration(List_All[4].inital_speed, List_All[0].get_Tm_Uniform_speed(), List_All[4].get_T_resequencing())
 acceleration_4.append(acceleration)
@@ -290,11 +233,8 @@ acceleration_4.append(acceleration)
 print(acceleration_4)
 Energy_Consumption_4.append(get_Energy_Consumption(List_All[4].inital_speed, List_All[4].get_T_resequencing(), acceleration_4[3], List_All[3].get_Tm_Uniform_speed()))
 print(Energy_Consumption_4)
-
+# Print the final result
 print('----------------------------------')
 print("After sorting:")
 for i in range(n):
     print(i, ' Vehicle\'s name:', List_All[i].name,'      Time of arrive merge point:', List_All[i].get_Tm_Uniform_speed())
-
-for i in range(len(List_All)):
-    print(List_All[i].get_Tm_Uniform_speed())
